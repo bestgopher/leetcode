523e6c8aab8501419ccf51df32e852aa9a8d8c8f
bestgopher <84328409@qq.com>
2020-12-24 19:13:05 +0800

    construct-binary-search-tree-from-preorder-traversal

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leetcode
 leetcode by rust
+
+### 题目
+- 1008：前序遍历构造二叉搜索树
+    - [src](https://github.com/rustors/leetcode/blob/main/src/bin/construct-binary-search-tree-from-preorder-traversal.rs)
+    - [leetcode](https://leetcode-cn.com/problems/construct-binary-search-tree-from-preorder-traversal/)
+
---
+++ src/bin/construct-binary-search-tree-from-preorder-traversal.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let v = vec![1, 23, 2, 25, 12, 6];
+    Solution::bst_from_preorder(v);
+}
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::ops::Deref;
+
+struct Solution;
+
+impl Solution {
+    pub fn bst_from_preorder(preorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        if preorder.len() == 0 {
+            return None;
+        }
+        let root = Rc::new(RefCell::new(TreeNode::new(preorder[0])));
+
+        for &i in preorder[1..].iter() {
+            let mut n = Rc::clone(&root);
+            loop {
+                if n.borrow().val > i {
+                    if let Some(s) = &n.clone().borrow().left {
+                        n = Rc::clone(s);
+                        continue
+                    }
+                    n.borrow_mut().left.replace(Rc::new(RefCell::new(TreeNode::new(i))));
+                    break;
+                } else {
+                    if let Some(s) = n.clone().borrow().right.as_ref() {
+                        n = Rc::clone(s);
+                        continue;
+                    }
+                    n.borrow_mut().right.replace(Rc::new(RefCell::new(TreeNode::new(i))));
+                    break;
+                }
+            }
+        }
+        Some(root)
+    }
+}
