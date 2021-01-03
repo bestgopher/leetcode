1e8e50ae691a1dae787050562c1c7909ca58c383
bestgopher <84328409@qq.com>
2021-01-03 15:09:44 +0800

    remove leetcode use

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 ///! 根据src/bin中新加的题目生成相应的markdown文件
 use leetcode;
-use leetcode::write_to_readme;
 
 fn main() {
     // 获取新加的文件
@@ -8,6 +7,6 @@ fn main() {
 
     for i in files.iter() {
         let res = leetcode::get_question_msg(i);
-        write_to_readme(res);
+        leetcode::write_to_readme(res);
     }
 }
