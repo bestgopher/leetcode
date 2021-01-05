5b6d9ef9dca4252d59feda28f55f43515daa25c7
bestgopher <84328409@qq.com>
2021-01-05 14:36:15 +0800

    single-number

---
+++ src/bin/single-number.rs
@@ -0,0 +1,14 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut r = 0;
+
+        for &i in nums.iter() {
+            r ^= i;
+        }
+        r
+    }
+}
