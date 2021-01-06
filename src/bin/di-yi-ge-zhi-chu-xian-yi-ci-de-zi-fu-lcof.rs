0dc7897c775f33725f4aeeb2eb81ea2edfbb40be
bestgopher <84328409@qq.com>
2021-01-06 21:19:59 +0800

    di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof another method

--- src/bin/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.rs
+++ src/bin/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.rs
@@ -3,10 +3,12 @@ fn main() {}
 struct Solution;
 
 impl Solution {
+    /// 通过HashMap
     pub fn first_uniq_char(s: String) -> char {
         if s == "".to_string() {
             return ' ';
         }
+
         let mut m = std::collections::HashMap::new();
         for &i in s.as_bytes() {
             m.entry(i).and_modify(|x| *x += 1).or_insert(1);
@@ -17,6 +19,27 @@ impl Solution {
                 if *s == 1 {
                     return i as char;
                 }
+            }
+        }
+
+        ' '
+    }
+
+    /// 通过数组
+    pub fn first_uniq_char1(s: String) -> char {
+        if s == "".to_string() {
+            return ' ';
+        }
+
+        let mut a = [0; 26];
+
+        for &i in s.as_bytes() {
+            a[(i - 97) as usize] += 1;
+        }
+
+        for &i in s.as_bytes() {
+            if a[(i - 97) as usize] == 1 {
+                return i as char;
             }
         }
 
