3363bddab68088a32da3d54a4fbe9d75b7f59cd2
bestgopher <84328409@qq.com>
2021-01-06 21:36:47 +0800

    ugly-number another method

--- src/bin/ugly-number.rs
+++ src/bin/ugly-number.rs
@@ -28,4 +28,26 @@ impl Solution {
 
         true
     }
+
+    pub fn is_ugly1(num: i32) -> bool {
+        if num == 0 {
+            return false;
+        }
+
+        let mut num = num;
+
+        while num % 5 == 0 {
+            num /= 5;
+        }
+
+        while num % 3 == 0 {
+            num /= 3;
+        }
+
+        while num % 2 == 0 {
+            num /= 2;
+        }
+
+        num == 1
+    }
 }
