74882351b1a0e8cda1e81a16ed361588dfe6a15c
bestgopher <84328409@qq.com>
2021-01-07 12:36:57 +0800

    src/bin/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.rs

---
+++ src/bin/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.rs
@@ -0,0 +1,55 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn kth_largest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        if k == 0 {
+            return root.unwrap().borrow().val;
+        }
+        let mut v = Vec::<i32>::new();
+        Self::scan(root, &mut v, k as usize);
+
+        v[k as usize - 1]
+    }
+
+    fn scan(root: Option<Rc<RefCell<TreeNode>>>, val: &mut Vec<i32>, k: usize) {
+        let root = root.unwrap();
+        if RefCell::borrow(&root).right.is_some() {
+            let right = root.borrow_mut().right.take();
+            Self::scan(right, val, k);
+        }
+
+        val.push(root.borrow().val);
+        if val.len() == k {
+            return;
+        }
+
+        if RefCell::borrow(&root).left.is_some() {
+            let right = root.borrow_mut().left.take();
+            Self::scan(right, val, k);
+        }
+    }
+}
