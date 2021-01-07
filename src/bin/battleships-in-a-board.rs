3b29402fbc38b8ad834b929c1fee5b3f7860626a
bestgopher <84328409@qq.com>
2021-01-07 19:17:56 +0800

    update battleships-in-a-board

--- src/bin/battleships-in-a-board.rs
+++ src/bin/battleships-in-a-board.rs
@@ -11,11 +11,7 @@ impl Solution {
         for i in 0..board.len() {
             for j in 0..board[0].len() {
                 if board[i][j] == 'X' {
-                    if i > 0 && board[i - 1][j] == 'X' {
-                        continue;
-                    }
-
-                    if j > 0 && board[i][j - 1] == 'X' {
+                    if (i > 0 && board[i - 1][j] == 'X') || (j > 0 && board[i][j - 1] == 'X') {
                         continue;
                     }
                     num += 1;
