607bf034cfa2455998c7a6319857b83ec905b1b1
bestgopher <84328409@qq.com>
2021-01-09 11:01:46 +0800

    src/bin/qiu-12n-lcof.rs

---
+++ src/bin/qiu-12n-lcof.rs
@@ -0,0 +1,12 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn sum_nums(n: i32) -> i32 {
+        let mut n = n;
+        // 利用布尔短路的思想，n == 0 时，不会允许&&后面的表达式
+        n > 0 && (n += Self::sum_nums(n - 1)) == ();
+        n
+    }
+}
