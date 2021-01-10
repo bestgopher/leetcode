5ba2f8ad0b5e84b91b80ea25d69a5496fb4c2f1d
bestgopher <84328409@qq.com>
2021-01-10 14:45:53 +0800

    src/bin/airplane-seat-assignment-probability.rs

---
+++ src/bin/airplane-seat-assignment-probability.rs
@@ -0,0 +1,13 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn nth_person_gets_nth_seat(n: i32) -> f64 {
+        if n == 1 {
+            return 1f64;
+        }
+
+        0.5
+    }
+}
