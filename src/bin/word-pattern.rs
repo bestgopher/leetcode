46b1fbb4a1fda33bf0d1ae749c9f271fc2c9e35c
bestgopher <84328409@qq.com>
2021-01-10 23:02:35 +0800

    src/bin/word-pattern.rs

---
+++ src/bin/word-pattern.rs
@@ -0,0 +1,36 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        let mut m = std::collections::HashMap::<u8, &str>::new();
+        let mut m1 = std::collections::HashMap::<&str, u8>::new();
+        let s = s.split(' ').collect::<Vec<&str>>();
+        let pattern = pattern.as_bytes();
+
+        if s.len() != pattern.len() {
+            return false;
+        }
+
+        for i in 0..s.len() {
+            if let Some(&v) = m1.get(&s[i]) {
+                if v != pattern[i] {
+                    return false;
+                }
+            }
+
+            if let Some(&v) = m.get(&pattern[i]) {
+                if v != s[i] {
+                    return false;
+                }
+            }
+
+            m1.insert(s[i], pattern[i]);
+            m.insert(pattern[i], s[i]);
+        }
+
+
+        true
+    }
+}
