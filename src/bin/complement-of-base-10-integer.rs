f5e0b671318cd3f93efcc6a331ee30f57e6ca0c7
bestgopher <84328409@qq.com>
2021-01-12 19:47:26 +0800

    src/bin/complement-of-base-10-integer.rs

---
+++ src/bin/complement-of-base-10-integer.rs
@@ -0,0 +1,13 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn bitwise_complement(n: i32) -> i32 {
+        let mut ones = 1;
+        while ones < num {
+            ones = (ones << 1) + 1
+        }
+        ones^num
+    }
+}
