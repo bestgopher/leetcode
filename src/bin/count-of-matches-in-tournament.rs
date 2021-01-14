ad6e3ba3ae4a17471c87c59a311d8ff227554fb5
bestgopher <84328409@qq.com>
2021-01-14 10:45:40 +0800

    update count-of-matches-in-tournament

--- src/bin/count-of-matches-in-tournament.rs
+++ src/bin/count-of-matches-in-tournament.rs
@@ -25,6 +25,8 @@ impl Solution {
         count
     }
     /// 找规律，就n-1
+    /// n个选手，决定一个冠军，则需要淘汰n-1个选手
+    /// 每场淘汰1个选手，因此需要n-1场比赛，所以需要配对n-1次
     pub fn number_of_matches1(n: i32) -> i32 {
         n - 1
     }
