2a4db354b9302e9f2112c423204464e889a78836
bestgopher <84328409@qq.com>
2021-01-26 13:44:35 +0800

    src/bin/next-greater-node-in-linked-list.rs

---
+++ src/bin/next-greater-node-in-linked-list.rs
@@ -0,0 +1,52 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+impl Solution {
+    pub fn next_larger_nodes(mut head: Option<Box<ListNode>>) -> Vec<i32> {
+        let mut v = vec![];
+        while let Some(l) = head.take() {
+            v.push(l.val);
+            head = l.next;
+        }
+
+        let (mut stack, mut res) = (vec![], vec![0; v.len()]);
+
+        for i in 0..v.len() {
+            if stack.len() == 0 {
+                stack.push(i);
+            } else {
+                // while stack.len() > 0 && v[i] > v[*stack.last().unwrap()] {
+                // }
+
+                while let Some(&t) = stack.last() {
+                    if v[i] <= v[t] {
+                        break;
+                    }
+                    res[stack.pop().unwrap()] = v[i];
+                }
+
+                stack.push(i);
+            }
+        }
+
+        res
+    }
+}
