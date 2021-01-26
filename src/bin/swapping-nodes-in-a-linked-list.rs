41fc0c70aa8c48a0aa34fbd728e2a55a902dc1ea
bestgopher <84328409@qq.com>
2021-01-26 21:44:34 +0800

    src/bin/swapping-nodes-in-a-linked-list.rs

---
+++ src/bin/swapping-nodes-in-a-linked-list.rs
@@ -0,0 +1,71 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+impl Solution {
+    /// 普通方法，先获取所有的值，然后再组装新的链表返回
+    pub fn swap_nodes1(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        let mut v = vec![];
+
+        while let Some(i) = head.take() {
+            v.push(i.val);
+            head = i.next;
+        }
+        let n = v.len() - k as usize;
+        v.swap(k as usize - 1, n);
+
+        let mut head = None;
+        for i in (0..v.len()).rev() {
+            let mut node = ListNode::new(v[i]);
+            if head.is_none() {
+                head = Some(Box::new(node));
+            } else {
+                node.next = head.take();
+                head = Some(Box::new(node));
+            }
+        }
+
+        head
+    }
+
+    /// 快慢指针，先快指针到第k个值的时候，慢指针从第一个元素开始移动
+    /// 快指针到达最后一个元素的时候，慢指针刚好在倒数第k个元素的位置
+    pub fn swap_nodes(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        let mut index = 0;
+        let (mut fast, mut slow) = (head.as_ref(), head.as_ref());
+
+        while fast.is_some() {
+            if index <= k {
+                fast = fast.unwrap().next.as_ref();
+                index += 1;
+            } else {
+                slow = slow.unwrap().next.as_ref();
+            }
+        }
+
+        let f = fast.unwrap().val;
+        let s = slow.unwrap().val;
+
+        fast.unwrap().val = s;
+        slow.unwrap().val = f;
+
+        head
+    }
+}
