70a92cbf8d88b7b0d3881a8cd70e511a64a1492c
bestgopher <84328409@qq.com>
2021-01-28 10:39:15 +0800

    src/bin/count-good-triplets.rs

---
+++ src/bin/count-good-triplets.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("{}", !10);
+}
+
+struct Solution;
+
+impl Solution {
+    /// 穷举，暴力破解
+    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
+        let mut count = 0;
+        for i in 0..arr.len() {
+            for j in i + 1..arr.len() {
+                if !((arr[i] - arr[j]).abs() <= a) {
+                    continue;
+                }
+
+                for k in j + 1..arr.len() {
+                    if (arr[j] - arr[k]).abs() <= b && (arr[i] - arr[k]).abs() <= c {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        count
+    }
+}
