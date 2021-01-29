22585c6e39004a0816593722e1c583b77a292005
bestgopher <84328409@qq.com>
2021-01-29 10:51:27 +0800

    src/bin/remove-duplicates-from-sorted-array.rs

---
+++ src/bin/remove-duplicates-from-sorted-array.rs
@@ -0,0 +1,25 @@
+fn main() {
+    assert_eq!(5, Solution::remove_duplicates(&mut vec![0,0,1,1,1,2,2,3,3,4]))
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.len() == 0 || nums.len() == 1 {
+            return nums.len() as i32;
+        }
+
+        let mut slow = 0usize;
+
+        for i in 1..nums.len() {
+            if nums[i] == nums[slow] {
+                continue;
+            } else {
+                slow += 1;
+                nums[slow] = nums[i];
+            }
+        }
+        slow as i32 + 1
+    }
+}
