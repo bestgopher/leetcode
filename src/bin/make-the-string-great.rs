5db96bedea09c23a1a5d4785ff8aa22d163c28e6
bestgopher <84328409@qq.com>
2021-01-30 18:06:51 +0800

    src/bin/make-the-string-great.rs

---
+++ src/bin/make-the-string-great.rs
@@ -0,0 +1,23 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn make_good(s: String) -> String {
+        let mut v = vec![];
+        for &i in s.as_bytes() {
+            if v.is_empty() {
+                v.push(i);
+            } else {
+                let &m = v.last().unwrap();
+                if (m > i && m - i == 32) || (m < i && i - m == 32) {
+                    v.pop();
+                } else {
+                    v.push(i);
+                }
+            }
+        }
+
+        String::from_utf8(v).unwrap()
+    }
+}
