ae6e54339bb724b95a591d910b39a84917ff2f47
bestgopher <84328409@qq.com>
2021-01-31 12:09:23 +0800

    src/bin/merge-two-binary-trees.rs

---
+++ src/bin/merge-two-binary-trees.rs
@@ -0,0 +1,55 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn merge_trees(t1: Option<Rc<RefCell<TreeNode>>>, t2: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if t1.is_some() {
+            Self::scan(Some(Rc::clone(t1.as_ref().unwrap())), t2);
+            t1
+        } else {
+            t2
+        }
+    }
+
+    fn scan(t1: Option<Rc<RefCell<TreeNode>>>, t2: Option<Rc<RefCell<TreeNode>>>) {
+        if t1.is_none() {
+            return;
+        } else if t1.is_some() && t2.is_some() {
+            t1.as_ref().unwrap().borrow_mut().val += t2.as_ref().unwrap().borrow().val;
+            if t1.as_ref().unwrap().borrow().left.is_some() {
+                Self::scan(Some(Rc::clone(t1.as_ref().unwrap().borrow_mut().left.as_ref().unwrap())), Rc::clone(t2.as_ref().unwrap()).borrow_mut().left.take());
+            } else {
+                t1.as_ref().unwrap().borrow_mut().left = t2.as_ref().unwrap().borrow_mut().left.take();
+            }
+
+            if t1.as_ref().unwrap().borrow().right.is_some() {
+                Self::scan(Some(Rc::clone(Rc::clone(t1.as_ref().unwrap()).borrow_mut().right.as_ref().unwrap())), Rc::clone(t2.as_ref().unwrap()).borrow_mut().right.take());
+            } else {
+                t1.as_ref().unwrap().borrow_mut().right = t2.as_ref().unwrap().borrow_mut().right.take();
+            }
+        }
+    }
+}
