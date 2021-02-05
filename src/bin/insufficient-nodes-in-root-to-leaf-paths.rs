21aae4b60fbe351b9924162a94b9bedcf82b2ca3
bestgopher <84328409@qq.com>
2021-02-05 10:25:30 +0800

    src/bin/insufficient-nodes-in-root-to-leaf-paths.rs

---
+++ src/bin/insufficient-nodes-in-root-to-leaf-paths.rs
@@ -0,0 +1,60 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn sufficient_subset(root: Option<Rc<RefCell<TreeNode>>>, limit: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if root.is_none() {
+            return None;
+        }
+
+        {
+            if root.as_ref().unwrap().borrow().left.is_none() && root.as_ref().unwrap().borrow().right.is_none() {
+                return if root.as_ref().unwrap().borrow().val < limit {
+                    None
+                } else {
+                    root
+                }
+            }
+        }
+
+        let val = root.as_ref().unwrap().borrow_mut().val;
+
+        let left = Self::sufficient_subset(root.as_ref().unwrap().borrow_mut().left.take(), limit - val);
+        root.as_ref().unwrap().borrow_mut().left = left;
+
+        let right = Self::sufficient_subset(root.as_ref().unwrap().borrow_mut().right.take(), limit - val);
+        root.as_ref().unwrap().borrow_mut().right = right;
+
+        {
+            let r = root.as_ref().unwrap().borrow();
+            if r.left.is_none() && r.right.is_none() {
+                return None;
+            }
+        }
+
+        root
+    }
+}
