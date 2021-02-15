1dbd543ed73983d9101c3b3a0232fccb6ddb8946
bestgopher <84328409@qq.com>
2021-02-15 13:09:47 +0800

    update summary-ranges

--- src/bin/summary-ranges.rs
+++ src/bin/summary-ranges.rs
@@ -15,29 +15,21 @@ impl Solution {
         let mut v = vec![];
         let (mut slow, mut fast) = (0usize, 0usize);
 
-        while fast < nums.len() {
+        while fast <= nums.len() {
             if fast == 0 {
                 fast += 1;
                 continue;
             }
 
-            if nums[fast] - nums[fast - 1] == 1 {
-                fast += 1;
-            } else {
+            if fast == nums.len() || nums[fast] - nums[fast - 1] != 1 {
                 if fast - 1 == slow {
                     v.push(nums[fast - 1].to_string());
                 } else {
                     v.push(format!("{}->{}", nums[slow], nums[fast - 1]));
                 }
                 slow = fast;
-                fast += 1;
             }
-        }
-
-        if fast - 1 == slow {
-            v.push(nums[fast - 1].to_string());
-        } else {
-            v.push(format!("{}->{}", nums[slow], nums[fast - 1]));
+            fast += 1;
         }
 
         v
