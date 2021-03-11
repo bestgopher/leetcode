9ad6cb8d9f11e6a67219b35f4c94a1d02a971e56
bestgopher <84328409@qq.com>
2021-03-12 07:55:05 +0800

    src/bin/valid-parentheses.rs

---
+++ src/bin/valid-parentheses.rs
@@ -0,0 +1,26 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = vec![];
+
+        for i in s.chars() {
+            match i {
+                '(' | '{' | '[' => stack.push(i),
+                _ => {
+                    if let Some(x) = stack.pop() {
+                        if (x == '(' && i != ')') || (x == '{' && i != '}') || (x == '[' && i != ']') {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        stack.is_empty()
+    }
+}
