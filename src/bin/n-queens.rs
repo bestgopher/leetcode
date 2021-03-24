bab1a400216c26f53d319b43d4c972cf1fb831ea
bestgopher <84328409@qq.com>
2021-03-25 07:50:40 +0800

    src/bin/n-queens.rs

--- src/bin/n-queens.rs
+++ src/bin/n-queens.rs
@@ -4,72 +4,58 @@ struct Solution;
 
 impl Solution {
     pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
-        let mut r = Vec::new();
-        for i in 0..n as usize {
-            let mut v = vec![vec![b'.'; n as usize]; n as usize];
-            v[0][i] = b'Q';
-            if Self::queen(&mut v, 1) {
-                r.push(v.into_iter().map(|x| String::from_utf8(x).unwrap()).collect());
-            }
-        }
+        let mut r = Vec::<Vec<String>>::new();
+        let mut nums = vec![vec![b'.'; n as usize]; n as usize];
+        Self::queen(&mut nums, n, &mut r);
+
         r
     }
 
-    fn queen(nums: &mut Vec<Vec<u8>>, n: i32) -> bool {
-        if n as usize > nums.len() {
-            return true;
+    fn queen(nums: &mut Vec<Vec<u8>>, n: i32, r: &mut Vec<Vec<String>>) {
+        if n == 0 {
+            let mut x = Vec::with_capacity(nums.len());
+
+            for i in nums.iter() {
+                x.push(String::from_utf8(i.clone()).unwrap());
+            }
+            r.push(x);
+            return;
         }
-        for i in 0..nums.len() {
-            if Self::
+        let len = nums.len();
+
+        for i in 0..len {
+            if !Self::check(nums, len - n as usize, i) {
+                continue;
+            }
+            nums[len - n as usize][i] = b'Q';
+            Self::queen(nums, n - 1, r);
+            nums[len - n as usize][i] = b'.';
         }
     }
 
     // 检查坐标(i,j)是否可以放置
     fn check(nums: &Vec<Vec<u8>>, i: usize, j: usize) -> bool {
-        let (n1, n2) = (nums[i].len(), nums.len());
-        for index in 0..n1 {
-            // 检查第i行
-            if nums[i][index] == b'Q' {
-                return false;
-            }
-        }
+        let n2 = nums.len();
 
-        for index in 0..n2 {
-            // 检查第i行
+        for index in 0..i {
+            // 检查同列
             if nums[index][j] == b'Q' {
                 return false;
             }
         }
 
-        // 检查主对角线
+        // 检查主对角线上方
         let mut offset = 1;
-        while i - offset >= 0 && j - offset >= 0 {
+        while i >= offset && j >= offset {
             if nums[i - offset][j - offset] == b'Q' {
                 return false;
             }
             offset += 1;
         }
-
+        // 检查副对角线上方
         let mut offset = 1;
-        while i + offset < n1 && j + offset < n2 {
-            if nums[i + offset][i + offset] == b'Q' {
-                return false;
-            }
-            offset += 1;
-        }
-
-        // 检查副对角线
-        let mut offset = 1;
-        while i + offset < n1 && j - offset >= 0 {
-            if nums[i + offset][j - offset] == b'Q' {
-                return false;
-            }
-            offset += 1;
-        }
-
-        let mut offset = 1;
-        while i - offset >= 0 && j + offset < n2 {
-            if nums[i - offset][i + offset] == b'Q' {
+        while i >= offset && j + offset < n2 {
+            if nums[i - offset][j + offset] == b'Q' {
                 return false;
             }
             offset += 1;
