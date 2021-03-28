817e9a2c4eaed98fb0c11f5cdd7f4481dc18aa60
bestgopher <84328409@qq.com>
2021-03-28 14:50:09 +0800

    src/bin/jump-game.rs

---
+++ src/bin/jump-game.rs
@@ -0,0 +1,23 @@
+fn main() {
+    assert_eq!(true, Solution::can_jump(vec![2, 3, 1, 1, 4]));
+    assert_eq!(false, Solution::can_jump(vec![3, 2, 1, 0, 4]));
+    assert_eq!(true, Solution::can_jump(vec![0]));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut max_position = 0;
+
+        for i in 0..nums.len() {
+            if i > max_position {
+                return false;
+            }
+
+            max_position = max_position.max(i + nums[i] as usize);
+        }
+
+        true
+    }
+}
