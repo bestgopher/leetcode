25438d95f6a0b24f4fd557271b02f196f8784d0d
bestgopher <84328409@qq.com>
2021-04-04 23:42:10 +0800

    README.md

--- README.md
+++ README.md
@@ -54,7 +54,7 @@
 - 20：有效的括号
     - [src](https://github.com/rustors/leetcode/blob/main/src/bin/valid-parentheses.rs)
     - [leetcode](https://leetcode-cn.com/problems/valid-parentheses/)
-- 26：删除排序数组中的重复项
+- 26：删除有序数组中的重复项
     - [src](https://github.com/rustors/leetcode/blob/main/src/bin/remove-duplicates-from-sorted-array.rs)
     - [leetcode](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/)
 - 27：移除元素
--- src/bin/unique-paths.rs
+++ src/bin/unique-paths.rs
@@ -6,16 +6,46 @@ struct Solution;
 
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let mut v = vec![vec![0; m as usize]; n as usize];
+        let mut v = vec![vec![0; n as usize]; m as usize];
 
-        Self::calc(&mut v, m, n);
+        Self::calc(&mut v, 0, 0);
 
         v[0][0]
     }
 
-    fn calc(array: &mut Vec<Vec<i32>>, m: i32, n: i32) {
+    fn calc(array: &mut Vec<Vec<i32>>, m: usize, n: usize) {
+        let s1 = array.len() - 1;
+        let s2 = array[0].len() - 1;
 
+        if m == s1 && n == s2 {
+            array[s1][s2] = 1;
+            return;
+        }
 
+        if array[m][n] != 0 {
+            return;
+        }
 
+        if m == s1 {
+            if array[m][n + 1] == 0 {
+                Self::calc(array, m, n + 1);
+            }
+            array[m][n] = array[m][n + 1];
+        } else if n == s2 {
+            if array[m + 1][n] == 0 {
+                Self::calc(array, m + 1, n);
+            }
+            array[m][n] = array[m + 1][n];
+        } else {
+            if array[m + 1][n] == 0 {
+                Self::calc(array, m + 1, n);
+            }
+
+            if array[m][n + 1] == 0 {
+                Self::calc(array, m, n + 1);
+            }
+
+            array[m][n] = array[m + 1][n] + array[m][n + 1];
+        }
     }
 }
