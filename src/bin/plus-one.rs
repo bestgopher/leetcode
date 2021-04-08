5098b21f9c47c95321f01ebf77e3ea8dae4de3a5
bestgopher <84328409@qq.com>
2021-04-08 21:49:39 +0800

    src/bin/plus-one.rs

---
+++ src/bin/plus-one.rs
@@ -0,0 +1,36 @@
+fn main() {
+    println!("{:?}", Solution::plus_one(vec![1]));
+    println!("{:?}", Solution::plus_one(vec![9]));
+    println!("{:?}", Solution::plus_one(vec![9, 9]));
+    println!("{:?}", Solution::plus_one(vec![1, 9]));
+    println!("{:?}", Solution::plus_one(vec![1, 9, 9]));
+    println!("{:?}", Solution::plus_one(vec![1, 2, 3]));
+    println!("{:?}", Solution::plus_one(vec![4, 3, 2, 1]));
+    println!("{:?}", Solution::plus_one(vec![0]));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+        let mut digits = digits;
+        let l = digits.len() - 1;
+        // 当最后一位不为9时
+        if digits[l] != 9 {
+            digits[l] += 1;
+        } else {
+            for i in (0..=l).rev() {
+                if digits[i] != 9 {
+                    digits[i] += 1;
+                    break;
+                }
+                digits[i] = 0;
+                if i == 0 {
+                    digits[i] += 1;
+                    digits.push(0);
+                }
+            }
+        }
+        digits
+    }
+}
