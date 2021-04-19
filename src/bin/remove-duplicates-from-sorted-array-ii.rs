8bd371d48134de0699e39bff3b5667419a0416a4
bestgopher <84328409@qq.com>
2021-04-19 21:51:16 +0800

    src/bin/remove-duplicates-from-sorted-array-ii.rs

---
+++ src/bin/remove-duplicates-from-sorted-array-ii.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut v = vec![0, 0, 1, 1, 1, 1, 2, 3, 3];
+    let s = Solution::remove_duplicates(&mut v);
+    println!("{:?}", &v[..s as usize]);
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let (mut index1, mut num) = (0, 0);
+
+        for i in 0..nums.len() {
+            if i == 0 || nums[i] == nums[i - 1] {
+                num += 1;
+            } else {
+                num = 1;
+            }
+
+            if num <= 2 {
+                nums[index1] = nums[i];
+                index1 += 1;
+                continue;
+            }
+        }
+
+        index1 as i32
+    }
+}
