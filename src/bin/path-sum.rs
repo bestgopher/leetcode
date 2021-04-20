0273dfeef98cf9a89a1b2cf139bc03cff4998025
bestgopher <84328409@qq.com>
2021-04-20 21:26:35 +0800

    src/bin/path-sum.rs

---
+++ src/bin/path-sum.rs
@@ -0,0 +1,42 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        if let Some(r) = root {
+            let value = Rc::clone(&r).borrow().val;
+            let left = Rc::clone(&r).borrow_mut().left.take();
+            let right = Rc::clone(&r).borrow_mut().right.take();
+
+            if left.is_none() && right.is_none() && value == target_sum {
+                return true;
+            }
+
+            return Self::has_path_sum(left, target_sum - value) || Self::has_path_sum(right, target_sum - value);
+        }
+        false
+    }
+}
