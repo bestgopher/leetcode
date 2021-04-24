71ea7d9e9de1a8905dcc61d29a2f56ef11be74ee
bestgopher <84328409@qq.com>
2021-04-24 20:52:43 +0800

    src/bin/combinations.rs

---
+++ src/bin/combinations.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("{:?}", Solution::combine(4, 2));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        Self::get(1, n, k)
+    }
+
+    fn get(start: i32, end: i32, k: i32) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+
+        for i in (start..=end).rev() {
+            if k != 1 {
+                for mut j in Self::get(start, i - 1, k - 1) {
+                    j.push(i);
+                    result.push(j);
+                }
+            } else {
+                result.push(vec![i]);
+            }
+        }
+        result
+    }
+}
