204f6424a4824fb299ada06dcd577cd508c6dfa3
bestgopher <84328409@qq.com>
2021-04-26 21:00:33 +0800

    src/bin/pascals-triangle.rs

---
+++ src/bin/pascals-triangle.rs
@@ -0,0 +1,24 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut result: Vec<Vec<i32>> = Vec::with_capacity(num_rows as usize);
+
+        for i in 1..=num_rows {
+            let mut r = Vec::with_capacity(i as usize);
+            for j in 0..i {
+                if j == 0 || j == i - 1 {
+                    r.push(1);
+                } else {
+                    r.push(result[i as usize - 2][j as usize - 1] + result[i as usize - 2][j as usize]);
+                }
+            }
+
+            result.push(r);
+        }
+
+        result
+    }
+}
