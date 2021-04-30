3673e919613d50da3a43f97cfd66e78bbc79e280
bestgopher <84328409@qq.com>
2021-04-30 19:57:18 +0800

    src/bin/binary-tree-inorder-traversal.rs

---
+++ src/bin/binary-tree-inorder-traversal.rs
@@ -0,0 +1,55 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut r = vec![];
+
+        Self::f(root, &mut r);
+
+        r
+    }
+
+    fn f(root: Option<Rc<RefCell<TreeNode>>>, r: &mut Vec<i32>) {
+        if root.is_none() {
+            return;
+        }
+
+        let root = root.unwrap();
+        let v = root.borrow().val;
+        let left = root.borrow_mut().left.take();
+        if left.is_some() {
+            Self::f(left, r);
+        }
+
+        r.push(v);
+
+        let right = root.borrow_mut().right.take();
+        if right.is_some() {
+            Self::f(right, r);
+        }
+    }
+}
