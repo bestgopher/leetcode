28d26f40da39a0bc831cc19958455aad8b483ddc
bestgopher <84328409@qq.com>
2021-05-06 21:27:20 +0800

    src/bin/unique-binary-search-trees-ii.rs

---
+++ src/bin/unique-binary-search-trees-ii.rs
@@ -0,0 +1,57 @@
+fn main() {
+    println!("{:?}", Solution::generate_trees(3))
+}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn generate_trees(n: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+        Self::f(1, n)
+    }
+
+    fn f(start: i32, end: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+        if start > end {
+            return vec![None];
+        }
+
+        let mut result = vec![];
+
+        for i in start..=end {
+            let left = Self::f(start, i - 1);
+            let right = Self::f(i + 1, end);
+
+            for j in &left {
+                for k in &right {
+                    let mut node = TreeNode::new(i);
+                    node.left = j.clone();
+                    node.right = k.clone();
+                    result.push(Some(Rc::new(RefCell::new(node))));
+                }
+            }
+        }
+
+        result
+    }
+}
