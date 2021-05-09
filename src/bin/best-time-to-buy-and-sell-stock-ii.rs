3c639530bc42efc4b39edafaf0b63cdbc37cc216
bestgopher <84328409@qq.com>
2021-05-09 14:51:44 +0800

    src/bin/best-time-to-buy-and-sell-stock-ii.rs

---
+++ src/bin/best-time-to-buy-and-sell-stock-ii.rs
@@ -0,0 +1,22 @@
+fn main() {
+    assert_eq!(7, Solution::max_profit(vec![7, 1, 5, 3, 6, 4]));
+    assert_eq!(4, Solution::max_profit(vec![1, 2, 3, 4, 5]));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let (mut r, mut min) = (0, prices[0]);
+
+        for i in 1..prices.len() {
+            if prices[i] < prices[i - 1] {
+                min = prices[i];
+            } else {
+                r += prices[i] - min;
+                min = prices[i];
+            }
+        }
+        r
+    }
+}
