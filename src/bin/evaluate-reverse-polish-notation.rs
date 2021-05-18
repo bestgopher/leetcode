9daa452ec81cc5a460b5e2e7229a8043f3b275b2
bestgopher <84328409@qq.com>
2021-05-18 20:42:37 +0800

    src/bin/evaluate-reverse-polish-notation.rs

---
+++ src/bin/evaluate-reverse-polish-notation.rs
@@ -0,0 +1,28 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+        let mut v = vec![];
+
+        for i in tokens.into_iter() {
+            if let Ok(i) = i.parse::<i32>() {
+                v.push(i);
+            } else {
+                let (x1, x2) = (v.pop().unwrap(), v.pop().unwrap());
+
+                let x = match i.as_str() {
+                    "+" => x1 + x2,
+                    "-" => x2 - x1,
+                    "*" => x1 * x2,
+                    "/" => x2 / x1,
+                    _ => 0
+                };
+
+                v.push(x);
+            }
+        }
+        v[0]
+    }
+}
