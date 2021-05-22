769cf5771c8bb38c33d445e9e0e15026910db430
bestgopher <84328409@qq.com>
2021-05-22 23:38:34 +0800

    src/bin/construct-binary-tree-from-preorder-and-inorder-traversal.rs

---
+++ src/bin/construct-binary-tree-from-preorder-and-inorder-traversal.rs
@@ -0,0 +1,52 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::build(&preorder[..], &inorder[..])
+    }
+
+    fn build(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if preorder.len() == 0 {
+            return None;
+        }
+
+        let mut root = TreeNode::new(preorder[0]);
+
+        let mut s = 0;
+        for (i, &v) in inorder.iter().enumerate() {
+            if v == preorder[0] {
+                s = i;
+                break;
+            }
+        }
+
+        root.left = Self::build(&preorder[1..1 + s], &inorder[..s]);
+        root.right = Self::build(&preorder[1 + s..], &inorder[1 + s..]);
+
+        Some(Rc::new(RefCell::new(root)))
+    }
+}
