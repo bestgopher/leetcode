94c5bad2ed7b576f76aaa86448727c4bb74754b4
bestgopher <84328409@qq.com>
2021-06-04 21:03:29 +0800

    src/bin/number-of-1-bits.rs

---
+++ src/bin/number-of-1-bits.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn hammingWeight(n: u32) -> i32 {
+        let mut r = 0;
+        let mut n = n;
+        while n > 0 {
+            if n % 2 == 1 {
+                r += 1;
+            }
+            n /= 2;
+        }
+
+        r
+    }
+}
