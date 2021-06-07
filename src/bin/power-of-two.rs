d6e48d5650f1c0327f14a02d449e772a5db2a0f6
bestgopher <84328409@qq.com>
2021-06-07 19:32:34 +0800

    src/bin/power-of-two.rs

---
+++ src/bin/power-of-two.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("{}", Solution::is_power_of_two(16));
+    println!("{}", Solution::is_power_of_two(15));
+    println!("{}", Solution::is_power_of_two(14));
+    println!("{}", Solution::is_power_of_two(5));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        n > 0 && (n - 1) & n == 0
+    }
+}
