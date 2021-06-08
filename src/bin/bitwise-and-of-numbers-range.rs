5a68f98a7aa0bec7b49a886f49518912c99e435d
bestgopher <84328409@qq.com>
2021-06-08 22:50:27 +0800

    src/bin/bitwise-and-of-numbers-range.rs

---
+++ src/bin/bitwise-and-of-numbers-range.rs
@@ -0,0 +1,17 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+        let mut shift = 0;
+        let (mut m, mut n) = (left, right);
+        while m < n {
+            m >>= 1;
+            n >>= 1;
+            shift += 1;
+        }
+
+        m << shift
+    }
+}
