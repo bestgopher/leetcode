b9776003b796f9235fe30a6e7d0b893defc90b69
bestgopher <84328409@qq.com>
2021-06-12 00:33:36 +0800

    src/bin/happy-number.rs

---
+++ src/bin/happy-number.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("{}", Solution::is_happy(19));
+    println!("{}", Solution::is_happy(2));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        let mut n = n;
+
+        let mut h = std::collections::HashSet::new();
+        loop {
+            let mut r = 0;
+            while n > 0 {
+                r += (n % 10).pow(2);
+                n /= 10;
+            }
+            if r == 1 {
+                return true;
+            }
+
+            if h.contains(&r) {
+                return false;
+            }
+
+            h.insert(r);
+            n = r;
+        }
+    }
+}
