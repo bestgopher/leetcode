63bf56ea2075026f695e7fd954500f5d752cc4a5
bestgopher <84328409@qq.com>
2021-06-23 20:55:17 +0800

    src/bin/contains-duplicate.rs

---
+++ src/bin/contains-duplicate.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut hash = std::collections::HashSet::new();
+
+        for i in nums.into_iter() {
+            if hash.contains(&i) {
+                return true;
+            }
+
+            hash.insert(i);
+        }
+
+        false
+    }
+}
