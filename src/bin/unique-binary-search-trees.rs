61f4d50d940311e2ebfd2c63fb2a187817537a5e
bestgopher <84328409@qq.com>
2021-07-04 23:28:12 +0800

    src/bin/unique-binary-search-trees.rs

---
+++ src/bin/unique-binary-search-trees.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("{}", Solution::num_trees(3));
+    println!("{}", Solution::num_trees(4));
+    println!("{}", Solution::num_trees(19));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn num_trees(n: i32) -> i32 {
+        let mut v = vec![0; n as usize + 1];
+        v[0] = 1;
+        v[1] = 1;
+
+        for i in 2..=n as usize {
+            for j in 1..=i {
+                v[i] += v[j - 1] * v[i - j];
+            }
+        }
+
+        v[n as usize]
+    }
+}
