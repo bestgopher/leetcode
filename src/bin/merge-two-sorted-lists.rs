3d0a1c7083d0b8ae05b97e996312e9a17c6e01bd
bestgopher <84328409@qq.com>
2021-07-05 21:33:36 +0800

    src/bin/merge-two-sorted-lists.rs

---
+++ src/bin/merge-two-sorted-lists.rs
@@ -0,0 +1,39 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+impl Solution {
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (l1, l2) {
+            (Some(mut x), Some(mut y)) => {
+                if x.val <= y.val {
+                    x.next = Self::merge_two_lists(x.next, Some(y));
+                    Some(x)
+                } else {
+                    y.next = Self::merge_two_lists(Some(x), y.next);
+                    Some(y)
+                }
+            }
+            (None, Some(y)) => Some(y),
+            (Some(x), None) => Some(x),
+            (None, None) => None,
+        }
+    }
+}
