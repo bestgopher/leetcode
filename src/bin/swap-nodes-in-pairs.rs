a9a53b165d84e39818605660e0d18ecbf4562184
bestgopher <84328409@qq.com>
2021-07-06 21:09:47 +0800

    src/bin/swap-nodes-in-pairs.rs

---
+++ src/bin/swap-nodes-in-pairs.rs
@@ -0,0 +1,38 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+impl Solution {
+    pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match head {
+            Some(mut x) => {
+                match x.next.take() {
+                    Some(mut y) => {
+                        x.next = Self::swap_pairs(y.next.take());
+                        y.next = Some(x);
+                        Some(y)
+                    }
+                    None => Some(x)
+                }
+            }
+            None => None
+        }
+    }
+}
