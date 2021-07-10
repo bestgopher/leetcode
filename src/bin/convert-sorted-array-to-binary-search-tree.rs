a48f881b057e9feef056da82887ecf9c57fa3d51
bestgopher <84328409@qq.com>
2021-07-10 20:41:43 +0800

    src/bin/convert-sorted-array-to-binary-search-tree.rs

---
+++ src/bin/convert-sorted-array-to-binary-search-tree.rs
@@ -0,0 +1,44 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::build(&nums)
+    }
+
+    fn build(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if nums.len() == 0 {
+            return None;
+        }
+
+        let middle = nums.len() / 2;
+        let mut root = TreeNode::new(nums[middle]);
+        root.left = Self::build(&nums[0..middle]);
+        root.right = Self::build(&nums[middle + 1..nums.len()]);
+
+        Some(Rc::new(RefCell::new(root)))
+    }
+}
