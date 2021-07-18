5f9c14c96675fba0f72ca9ebf5d548f5b5aafcf4
bestgopher <84328409@qq.com>
2021-07-18 23:37:40 +0800

    src/bin/first-bad-version.rs

---
+++ src/bin/first-bad-version.rs
@@ -0,0 +1,35 @@
+fn main() {
+    // println!("{}", Solution::new(1702766719).first_bad_version(2126753390));
+    // println!("{}", Solution::new(1).first_bad_version(1));
+    println!("{}", Solution::new(2).first_bad_version(2));
+}
+
+struct Solution {
+    n: i32
+}
+
+// The API isBadVersion is defined for you.
+// isBadVersion(versions:i32)-> bool;
+// to call it use self.isBadVersion(versions)
+
+impl Solution {
+    fn new(n: i32) -> Self { Self { n } }
+
+    pub fn first_bad_version(&self, n: i32) -> i32 {
+        let mut good: i64 = 0;
+        let mut bad: i64 = n as i64;
+        while bad - good > 1 {
+            let mid = (good + bad + 1) / 2;
+            if self.isBadVersion(mid as i32) {
+                bad = mid;
+            } else {
+                good = mid;
+            }
+        }
+        bad as i32
+    }
+
+    fn isBadVersion(&self, versions: i32) -> bool {
+        self.n <= versions
+    }
+}
