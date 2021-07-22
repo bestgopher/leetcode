148c85cd26830b80a9088f894a2b3c0d6e66eaad
bestgopher <84328409@qq.com>
2021-07-22 21:26:34 +0800

    src/bin/count-numbers-with-unique-digits.rs

---
+++ src/bin/count-numbers-with-unique-digits.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("{}", Solution::count_numbers_with_unique_digits(2));
+    println!("{}", Solution::count_numbers_with_unique_digits(8));
+    println!("{}", Solution::count_numbers_with_unique_digits(7));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn count_numbers_with_unique_digits(n: i32) -> i32 {
+        if n == 0 {
+            return 1;
+        }
+
+        if n == 1 {
+            return 10;
+        }
+
+        let mut sum =9;
+
+        for i in 0..n - 1 {
+            sum *= 9 - i;
+        }
+
+        sum + Self::count_numbers_with_unique_digits(n - 1)
+    }
+}
