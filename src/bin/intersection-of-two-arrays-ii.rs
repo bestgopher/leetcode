4a8a4c1f069948fe0fcc01e906f2783756b16315
bestgopher <84328409@qq.com>
2021-07-26 20:52:31 +0800

    src/bin/intersection-of-two-arrays-ii.rs

---
+++ src/bin/intersection-of-two-arrays-ii.rs
@@ -0,0 +1,27 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut v = vec![];
+        let (mut nums1, mut nums2) = (nums1, nums2);
+        nums1.sort();
+        nums2.sort();
+        let (mut i, mut j) = (0, 0);
+
+        while i < nums1.len() && j < nums2.len() {
+            if nums1[i] == nums2[j] {
+                v.push(nums2[j]);
+                i += 1;
+                j += 1;
+            } else if nums1[i] < nums2[j] {
+                i += 1;
+            } else {
+                j += 1;
+            }
+        }
+
+        v
+    }
+}
