d19f899db719812e255ff420ae535c57d74e4391
bestgopher <84328409@qq.com>
2021-08-16 22:40:06 +0800

    src/bin/reverse-string.rs

---
+++ src/bin/reverse-string.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        if s.is_empty() {
+            return;
+        }
+        let (mut s1, mut s2) = (0, s.len() - 1);
+
+        while s1 < s2 {
+            s.swap(s1, s2);
+            s1 += 1;
+            s2 -= 1;
+        }
+    }
+}
