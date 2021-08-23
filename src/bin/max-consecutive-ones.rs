e55680b28db69bbda33021f037ca1712dd9563cb
bestgopher <84328409@qq.com>
2021-08-23 21:48:57 +0800

    src/bin/max-consecutive-ones.rs

---
+++ src/bin/max-consecutive-ones.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        let mut r = 0;
+        let mut count = 0;
+
+        for i in nums {
+            if i == 1 {
+                count += 1;
+            } else {
+                r = r.max(count);
+                count = 0
+            }
+        }
+        r
+    }
+}
