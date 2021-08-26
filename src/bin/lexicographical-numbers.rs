2b38318e882ba02aa78f99304c749c1fd2afacff
bestgopher <84328409@qq.com>
2021-08-27 00:06:40 +0800

    src/bin/lexicographical-numbers.rs

--- src/bin/lexicographical-numbers.rs
+++ src/bin/lexicographical-numbers.rs
@@ -3,7 +3,7 @@ fn main() {}
 struct Solution;
 
 impl Solution {
-    pub fn lexical_order(n: i32) -> Vec<i32> {
+    pub fn lexical_order1(n: i32) -> Vec<i32> {
         let mut s = (1..=n)
             .into_iter()
             .map(|x| x.to_string())
@@ -12,5 +12,28 @@ impl Solution {
         s.sort();
 
         s.into_iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+    }
+
+    pub fn lexical_order(n: i32) -> Vec<i32> {
+        let mut v = Vec::with_capacity(n as usize);
+
+        for i in 1..10.min(n + 1) {
+            v.push(i);
+            Self::dfs(&mut v, i, n);
+        }
+        v
+    }
+
+    fn dfs(v: &mut Vec<i32>, number: i32, n: i32) {
+        if number > n { return; }
+
+        let number = number * 10;
+
+        for i in 0..10 {
+            if number + i <= n {
+                v.push(number + i);
+                Self::dfs(v, number + i, n);
+            }
+        }
     }
 }
