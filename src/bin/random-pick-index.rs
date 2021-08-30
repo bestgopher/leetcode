ac3c93cca2af05ae083fb43ce7079f62f0252287
bestgopher <84328409@qq.com>
2021-08-30 08:41:26 +0800

    src/bin/random-pick-index.rs

--- src/bin/random-pick-index.rs
+++ src/bin/random-pick-index.rs
@@ -5,7 +5,7 @@ fn main() {}
  * let obj = Solution::new(nums);
  * let ret_1: i32 = obj.pick(target);
  */
-struct Solution {
+struct Solution1 {
     index_map: std::collections::HashMap<i32, Vec<usize>>
 }
 
@@ -14,7 +14,7 @@ struct Solution {
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
-impl Solution {
+impl Solution1 {
     fn new(nums: Vec<i32>) -> Self {
         let mut index_map = std::collections::HashMap::new();
 
@@ -38,5 +38,32 @@ impl Solution {
             }
             None => 0
         }
+    }
+}
+
+struct Solution {
+    nums: Vec<i32>
+}
+
+impl Solution {
+    fn new(nums: Vec<i32>) -> Self {
+        Self { nums }
+    }
+
+    fn pick(&self, target: i32) -> i32 {
+        use rand::Rng;
+        let mut n = 0;
+        let mut r = 0;
+
+        for i in 0..self.nums.len() {
+            if self.nums[i] == target {
+                n += 1;
+                if rand::thread_rng().gen_range(0..n) == 0 {
+                    r = i as i32;
+                }
+            }
+        }
+
+        r
     }
 }
