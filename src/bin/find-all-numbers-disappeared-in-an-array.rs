9a2abc2e4e0e89580d9ba7732c1518e8fb889d4d
bestgopher <84328409@qq.com>
2021-09-09 23:59:46 +0800

    src/bin/find-all-numbers-disappeared-in-an-array.rs

---
+++ src/bin/find-all-numbers-disappeared-in-an-array.rs
@@ -0,0 +1,16 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
+        let mut s = std::collections::HashMap::new();
+
+        for &i in nums.iter() {
+            s.insert(i, ());
+        }
+
+        let mut v= vec![];
+        for i in 1..=nums.len() {}
+    }
+}
