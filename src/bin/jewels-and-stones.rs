45466b5fe185c0249c33bfed223ba326593a450f
bestgopher <84328409@qq.com>
2021-09-24 23:49:35 +0800

    src/bin/jewels-and-stones.rs

---
+++ src/bin/jewels-and-stones.rs
@@ -0,0 +1,10 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn num_jewels_in_stones(jewels: String, stones: String) -> i32 {
+        let j = jewels.bytes().into_iter().collect::<std::collections::HashSet<u8>>();
+        stones.bytes().into_iter().filter(|x| j.contains(x)).count() as i32
+    }
+}
