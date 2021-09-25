4064969196b7253cbb28fecc45db215acf525c5e
bestgopher <84328409@qq.com>
2021-09-25 21:51:03 +0800

    src/bin/find-bottom-left-tree-value.rs

---
+++ src/bin/find-bottom-left-tree-value.rs
@@ -0,0 +1,51 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn find_bottom_left_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut stack = vec![root];
+        let mut result = 0;
+        while !stack.is_empty() {
+            let mut new_stack = vec![];
+            let mut flag = false;
+            for node in stack.into_iter() {
+                if let Some(x) = node {
+                    if !flag {
+                        result = x.borrow().val;
+                        flag = true;
+                    }
+                    new_stack.push(x.borrow_mut().left.take());
+                    new_stack.push(x.borrow_mut().right.take());
+                }
+            }
+
+            stack = new_stack;
+            flag = false;
+        }
+
+        result
+    }
+}
