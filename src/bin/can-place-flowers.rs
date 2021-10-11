e3d21f3a97a7f7f6204c2d97c82447074ef70c53
bestgopher <84328409@qq.com>
2021-10-11 18:28:06 +0800

    src/bin/can-place-flowers.rs

---
+++ src/bin/can-place-flowers.rs
@@ -0,0 +1,35 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
+        let mut f = flowerbed;
+        let mut n = n;
+
+        for i in 0..f.len() {
+            if i == 0 {
+                if f[i] == 0 && f.get(i + 1).unwrap_or(&0) == &0 {
+                    f[i] = 1;
+                    n -= 1;
+                }
+            } else if i == f.len() - 1 {
+                if f[i] == 0 && f.get(i - 1).unwrap_or(&0) == &0 {
+                    f[i] = 1;
+                    n -= 1;
+                }
+            } else {
+                if f[i] == 0 && f[i - 1] == 0 && f[i + 1] == 0 {
+                    f[i] = 1;
+                    n -= 1;
+                }
+            }
+
+            if n <= 0 {
+                return true;
+            }
+        }
+
+        false
+    }
+}
