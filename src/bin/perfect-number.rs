d2808950279b05785d45e35c22891c4f9540667a
bestgopher <84328409@qq.com>
2021-10-16 23:08:54 +0800

    src/bin/perfect-number.rs

---
+++ src/bin/perfect-number.rs
@@ -0,0 +1,9 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn check_perfect_number(num: i32) -> bool {
+        num == (1..=num / 2).into_iter().filter(|&x| num % x == 0).sum()
+    }
+}
