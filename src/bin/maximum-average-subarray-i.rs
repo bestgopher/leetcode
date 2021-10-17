34613c79ae57d9ecd4e6faac00fc437194fc2c65
bestgopher <84328409@qq.com>
2021-10-17 23:26:06 +0800

    src/bin/maximum-average-subarray-i.rs

---
+++ src/bin/maximum-average-subarray-i.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+        let k = k as usize;
+        let mut sum = nums[0..k].iter().sum::<i32>();
+        let mut argv = (sum as f64) / (k as f64);
+
+        for i in k..nums.len() {
+            sum = sum + nums[i] - nums[i - k];
+            argv = argv.max((sum as f64) / (k as f64));
+        }
+
+        argv
+    }
+}
