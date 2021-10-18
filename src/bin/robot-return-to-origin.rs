de2b1803eca65e4c4012c4f9610013a1badff5e4
bestgopher <84328409@qq.com>
2021-10-18 23:01:57 +0800

    src/bin/robot-return-to-origin.rs

---
+++ src/bin/robot-return-to-origin.rs
@@ -0,0 +1,22 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let mut v = (0, 0);
+
+        for i in moves.bytes() {
+            match i {
+                b'L' => v.0 -= 1,
+                b'R' => v.0 += 1,
+                b'U' => v.1 += 1,
+                b'D' => v.1 -= 1,
+                _ => {}
+            }
+        }
+
+
+        v == (0, 0)
+    }
+}
