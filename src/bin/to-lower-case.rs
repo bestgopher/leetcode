a842246f5d7a57847d60035f884384af09bed978
bestgopher <84328409@qq.com>
2021-12-12 10:23:25 +0800

    src/bin/to-lower-case.rs

---
+++ src/bin/to-lower-case.rs
@@ -0,0 +1,19 @@
+use serde::de::Unexpected::Str;
+
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        let mut new = Vec::with_capacity(s.len());
+        s.bytes().into_iter().for_each(|mut x| {
+            if x >= b'A' && x <= b'Z' {
+                x += 32;
+            }
+            new.push(x);
+        });
+
+        String::from_utf8(new).unwrap()
+    }
+}
