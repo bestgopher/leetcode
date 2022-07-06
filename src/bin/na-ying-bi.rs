d47d55ecdb2261df3c381a03ab3c7b900f6d2078
bestgopher <84328409@qq.com>
2022-07-06 22:42:59 +0800

    src/bin/na-ying-bi.rs

---
+++ src/bin/na-ying-bi.rs
@@ -0,0 +1,11 @@
+fn main() {
+    assert_eq!(Solution::min_count(vec![4, 2, 1]), 4);
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn min_count(coins: Vec<i32>) -> i32 {
+        coins.into_iter().map(|x| x / 2 + x % 2).sum()
+    }
+}
