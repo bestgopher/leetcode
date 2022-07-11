af75adfd58792d148b367c00af5a18f619887cfd
bestgopher <84328409@qq.com>
2022-07-11 23:06:47 +0800

    src/bin/minimum-absolute-difference-in-bst.rs

--- src/bin/minimum-absolute-difference-in-bst.rs
+++ src/bin/minimum-absolute-difference-in-bst.rs
@@ -21,8 +21,8 @@ impl TreeNode {
     }
 }
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 impl Solution {
     pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
