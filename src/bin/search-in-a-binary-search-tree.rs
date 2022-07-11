820d7f32e49797ac40d88723c8a061be50fffe57
bestgopher <84328409@qq.com>
2022-07-11 23:06:49 +0800

    src/bin/search-in-a-binary-search-tree.rs

--- src/bin/search-in-a-binary-search-tree.rs
+++ src/bin/search-in-a-binary-search-tree.rs
@@ -21,11 +21,14 @@ impl TreeNode {
     }
 }
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 impl Solution {
-    pub fn search_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    pub fn search_bst(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
         let mut root = root;
         while let Some(r) = root {
             let v = r.borrow().val;
