46714ca69d3a4610b1f19c7379df316bc16f6e38
bestgopher <84328409@qq.com>
2022-07-11 23:06:45 +0800

    src/bin/insert-into-a-binary-search-tree.rs

--- src/bin/insert-into-a-binary-search-tree.rs
+++ src/bin/insert-into-a-binary-search-tree.rs
@@ -21,11 +21,14 @@ impl TreeNode {
     }
 }
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 impl Solution {
-    pub fn insert_into_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    pub fn insert_into_bst(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
         if root.is_none() {
             return Some(Rc::new(RefCell::new(TreeNode::new(val))));
         }
