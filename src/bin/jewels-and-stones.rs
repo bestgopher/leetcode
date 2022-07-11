23b5bb0320fee20a2649c9e038666e96e3cb245d
bestgopher <84328409@qq.com>
2022-07-11 23:06:46 +0800

    src/bin/jewels-and-stones.rs

--- src/bin/jewels-and-stones.rs
+++ src/bin/jewels-and-stones.rs
@@ -4,7 +4,10 @@ struct Solution;
 
 impl Solution {
     pub fn num_jewels_in_stones(jewels: String, stones: String) -> i32 {
-        let j = jewels.bytes().into_iter().collect::<std::collections::HashSet<u8>>();
+        let j = jewels
+            .bytes()
+            .into_iter()
+            .collect::<std::collections::HashSet<u8>>();
         stones.bytes().into_iter().filter(|x| j.contains(x)).count() as i32
     }
 }
