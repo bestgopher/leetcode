b654c9327a307af5e63c01ace28f112d8d41f966
bestgopher <84328409@qq.com>
2022-07-17 23:22:25 +0800

    src/bin/ti-huan-kong-ge-lcof.rs

---
+++ src/bin/ti-huan-kong-ge-lcof.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn replace_space(s: String) -> String {
+        let mut result = String::new();
+        for i in s.chars() {
+            if i == ' ' {
+                result.push_str("%20");
+            } else {
+                result.push(i);
+            }
+        }
+
+        result
+    }
+}
