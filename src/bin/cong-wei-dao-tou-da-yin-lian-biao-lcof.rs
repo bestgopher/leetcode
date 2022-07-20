94c8d9f0f28247e847e2bc0f2997a7e35fed118f
bestgopher <84328409@qq.com>
2022-07-20 21:19:14 +0800

    src/bin/cong-wei-dao-tou-da-yin-lian-biao-lcof.rs

---
+++ src/bin/cong-wei-dao-tou-da-yin-lian-biao-lcof.rs
@@ -0,0 +1,34 @@
+fn main() {}
+
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn reverse_print(head: Option<Box<ListNode>>) -> Vec<i32> {
+        let mut result = vec![];
+        let mut head = head;
+
+        while head.is_some() {
+            let h = head.unwrap();
+            result.push(h.val);
+            head = h.next;
+        }
+
+        result.reverse();
+
+        result
+    }
+}
