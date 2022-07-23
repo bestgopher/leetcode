a56b77a40e43248e1ff4a0696eaffcd6ad024497
bestgopher <84328409@qq.com>
2022-07-23 12:11:53 +0800

    src/bin/count-numbers-with-unique-digits.rs

--- src/bin/count-numbers-with-unique-digits.rs
+++ src/bin/count-numbers-with-unique-digits.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused, unused_variables)]
+
 fn main() {
     println!("{}", Solution::count_numbers_with_unique_digits(2));
     println!("{}", Solution::count_numbers_with_unique_digits(8));
