71874248982db01db0907f87863b5d28fce1db9f
bestgopher <84328409@qq.com>
2022-07-23 12:23:00 +0800

    src/bin/first-bad-version.rs

--- src/bin/first-bad-version.rs
+++ src/bin/first-bad-version.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code, unused, unused_variables)]
+#![allow(dead_code, unused, unused_variables, non_snake_case)]
 
 fn main() {
     // println!("{}", Solution::new(1702766719).first_bad_version(2126753390));
