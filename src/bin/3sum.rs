a9ccbaac78cf65fd9d775b9893c19f911af058d5
bestgopher <84328409@qq.com>
2022-07-23 11:43:28 +0800

    src/bin/3sum.rs

--- src/bin/3sum.rs
+++ src/bin/3sum.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused, unused_variables)]
+
 fn main() {
     // println!("{:?}", Solution::three_sum(vec![0, 0, 0, 0]));
     // println!("{:?}", Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]));
