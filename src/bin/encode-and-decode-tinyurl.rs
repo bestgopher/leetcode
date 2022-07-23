638b74932dcc0435960b410fdf50e7a0d960ec7b
bestgopher <84328409@qq.com>
2022-07-23 12:23:00 +0800

    src/bin/encode-and-decode-tinyurl.rs

--- src/bin/encode-and-decode-tinyurl.rs
+++ src/bin/encode-and-decode-tinyurl.rs
@@ -26,18 +26,18 @@ impl Codec {
     }
 
     // Encodes a URL to a shortened URL.
-    fn encode(&mut self, longURL: String) -> String {
+    fn encode(&mut self, long_url: String) -> String {
         let mut s = std::collections::hash_map::DefaultHasher::new();
-        <String as std::hash::Hash>::hash(&longURL, &mut s);
+        <String as std::hash::Hash>::hash(&long_url, &mut s);
         let hash_code =
             <std::collections::hash_map::DefaultHasher as std::hash::Hasher>::finish(&s);
-        self.hash.insert(hash_code, longURL);
+        self.hash.insert(hash_code, long_url);
         format!("http://tinyurl.com/{}", hash_code)
     }
 
     // Decodes a shortened URL to its original URL.
-    fn decode(&self, shortURL: String) -> String {
-        let hash_code = shortURL
+    fn decode(&self, short_url: String) -> String {
+        let hash_code = short_url
             .trim_start_matches("http://tinyurl.com/")
             .parse::<u64>()
             .unwrap();
