7f3975239f4736859ff523b3d93c838501e53dce
bestgopher <84328409@qq.com>
2023-02-01 15:21:34 +0800

    src/bin/sudoku-solver.rs

--- src/bin/sudoku-solver.rs
+++ src/bin/sudoku-solver.rs
@@ -29,51 +29,50 @@ impl Solution {
     }
 
     pub fn rec(board: &mut Vec<Vec<char>>, start: (usize, usize)) -> bool {
+        if start.0 > 8 || start.1 > 8 {
+            return false;
+        }
+
         if start.0 == 8 && start.1 == 8 && board[start.0][start.1] != '.' {
             return true;
         }
 
-        let mut index = (start.0, start.1);
-        while index.0 < 9 && index.1 < 9 {
-            if board[index.0][index.1] == '.' {
-                break;
-            }
-            index.1 += 1;
-            if index.1 == 9 {
-                index.0 += 1;
-                index.1 = 0;
-            }
-        }
+        // 下一个坐标
+        let mut index = if start.1 == 8 {
+            (start.0 + 1, 0)
+        } else {
+            (start.0, start.1 + 1)
+        };
 
-        if index.0 == 9 || index.1 == 9 {
-            return true;
+        if board[start.0][start.1] != '.' {
+            return Self::rec(board, index);
         }
 
         'Loop: for value in '1'..='9' {
             // 检查行和列
             for x in 0..9 {
-                if board[index.0][x] == value {
+                if board[start.0][x] == value {
                     continue 'Loop;
                 }
 
-                if board[x][index.1] == value {
+                if board[x][start.1] == value {
                     continue 'Loop;
                 }
             }
 
             // 检查四周
-            for i in index.0 / 3 * 3..index.0 / 3 * 3 + 3 {
-                for j in index.1 / 3 * 3..index.1 / 3 * 3 + 3 {
+            for i in start.0 / 3 * 3..start.0 / 3 * 3 + 3 {
+                for j in start.1 / 3 * 3..start.1 / 3 * 3 + 3 {
                     if board[i][j] == value {
                         continue 'Loop;
                     }
                 }
             }
 
-            board[index.0][index.1] = value;
+            board[start.0][start.1] = value;
 
             if !Solution::rec(board, index) {
-                board[index.0][index.1] = '.';
+                board[start.0][start.1] = '.';
             } else {
                 return true;
             }
