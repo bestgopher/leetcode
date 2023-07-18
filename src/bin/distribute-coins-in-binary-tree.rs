17a5d311a507fcf6efaac4b86fa291d45f4e8544
bestgopher <84328409@qq.com>
2023-07-18 20:34:24 +0800

    src/bin/distribute-coins-in-binary-tree.rs

---
+++ src/bin/distribute-coins-in-binary-tree.rs
@@ -0,0 +1,35 @@
+#![allow(dead_code, unused, unused_variables, non_snake_case)]
+
+fn main() {}
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn distribute_coins(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut r = 0;
+
+        r
+    }
+}
