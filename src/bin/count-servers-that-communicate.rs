cd6309349c392d048e16d4d7e462d4cf8ed5dd65
bestgopher <84328409@qq.com>
2023-08-24 21:05:09 +0800

    src/bin/count-servers-that-communicate.rs

---
+++ src/bin/count-servers-that-communicate.rs
@@ -0,0 +1,41 @@
+#![allow(dead_code, unused, unused_variables, non_snake_case)]
+
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn count_servers(grid: Vec<Vec<i32>>) -> i32 {
+        let (mut row, mut col) = (
+            std::collections::HashMap::new(),
+            std::collections::HashMap::new(),
+        );
+
+        let mut result = 0;
+
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                if grid[i][j] == 1 {
+                    row.entry(i).and_modify(|x| *x += 1).or_insert(1);
+                    col.entry(j).and_modify(|x| *x += 1).or_insert(1);
+                }
+            }
+        }
+
+        println!("{row:?}");
+        println!("{col:?}");
+
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                if grid[i][j] == 1 {
+                    match (row.get(&i), col.get(&j)) {
+                        (Some(&x), Some(&y)) if x > 1 || y > 1 => result += 1,
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        result
+    }
+}
