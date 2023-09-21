10de83cf6ea67e1797d19effcce657b13fd02d92
bestgopher <84328409@qq.com>
2023-09-21 20:51:01 +0800

    src/bin/find-the-maximum-achievable-number.rs

---
+++ src/bin/find-the-maximum-achievable-number.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code, unused, unused_variables, non_snake_case)]
+
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+        num + t * 2
+    }
+}
