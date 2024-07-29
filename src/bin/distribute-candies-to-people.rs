541b3d6c61d733a4eca46e5275af22977d224f82
bestgopher <84328409@qq.com>
2024-07-29 21:20:51 +0800

    src/bin/distribute-candies-to-people.rs

--- src/bin/distribute-candies-to-people.rs
+++ src/bin/distribute-candies-to-people.rs
@@ -6,20 +6,16 @@ struct Solution;
 
 impl Solution {
     pub fn distribute_candies(candies: i32, num_people: i32) -> Vec<i32> {
-        impl Solution {
-            pub fn distribute_candies(candies: i32, n: i32) -> Vec<i32> {
-                let m = (((8.0 * candies as f64 + 1.0).sqrt() - 1.0) / 2.0) as i32;
-                let k = m / n;
-                let extra = m % n;
-                let mut ans = (0..n)
-                    .map(|i| {
-                        let k = if i < extra { k + 1 } else { k };
-                        k * (k - 1) / 2 * n + k * (i + 1)
-                    })
-                    .collect::<Vec<_>>();
-                ans[extra as usize] += candies - m * (m + 1) / 2;
-                ans
-            }
-        }
+        let m = (((8.0 * candies as f64 + 1.0).sqrt() - 1.0) / 2.0) as i32;
+        let k = m / num_people;
+        let extra = m % num_people;
+        let mut ans = (0..num_people)
+            .map(|i| {
+                let k = if i < extra { k + 1 } else { k };
+                k * (k - 1) / 2 * num_people + k * (i + 1)
+            })
+            .collect::<Vec<_>>();
+        ans[extra as usize] += candies - m * (m + 1) / 2;
+        ans
     }
 }
